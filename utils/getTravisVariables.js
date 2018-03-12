ea3e1d0a705b43254fa2d01e05c153a68cca216c
CPatchane <code@patchane.com>
2018-03-12 01:25:32 +0100

    feat: greatly improve publishing scripts and code outline :art:

---
+++ utils/getTravisVariables.js
@@ -0,0 +1,17 @@
+function getTravisVariables () {
+  // values from process.env are always string
+  const getEnv = (name) => process.env[name]
+
+  return {
+    TRAVIS_PULL_REQUEST: getEnv('TRAVIS_PULL_REQUEST'),
+    TRAVIS_BRANCH: getEnv('TRAVIS_BRANCH'),
+    TRAVIS_BUILD_DIR: getEnv('TRAVIS_BUILD_DIR'),
+    TRAVIS_TAG: getEnv('TRAVIS_TAG'),
+    TRAVIS_COMMIT: getEnv('TRAVIS_COMMIT'),
+    TRAVIS_REPO_SLUG: getEnv('TRAVIS_REPO_SLUG'),
+    // encrypted variables
+    REGISTRY_TOKEN: getEnv('REGISTRY_TOKEN')
+  }
+}
+
+module.exports = getTravisVariables
